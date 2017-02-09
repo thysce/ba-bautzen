0665fde6b2715b879d39f1ae3d90c8be8b9d8196
Tim Trense <Tim Trense@TimTrense>
2017-02-09 12:15:42 +0100

    documented and fixed little bugs

--- Statistix/src/org/wi16/stat/Simplex.java
+++ Statistix/src/org/wi16/stat/Simplex.java
@@ -1,13 +1,40 @@
 package org.wi16.stat;
 
-import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 
+/**
+ * implementing the simplex-algorithm for searching optimal values such that a
+ * term becomes maximal under certain conditions
+ * 
+ * @see #Simplex(double[], double[][])
+ * @see #getOptimum()
+ * @see #getOptimalValues()
+ * 
+ * @author Tim Trense
+ */
 public class Simplex
 {
 	private final double[][] tableau;
 	private final int[] res;
 	private boolean done;
 
+	// helping values
+	final int zfz;
+	final int q;
+	final int b;
+
+	/**
+	 * constructs a simplex-task searching the optimal x1, x2, ..., xn for
+	 * ax1+bx2+...+zxn to become maximal under the conditions:<br>
+	 * a1x1+b1x2+...+z1xn + w1= MAX1 & a2x1+b2x2+...+z2xn +w2= MAX2 & ...<br>
+	 * 
+	 * @param c
+	 *            the coefficients of the term ax1+bx2+...+zxn. [a,b,...,z]
+	 * @param nb
+	 *            the coefficient-matrix for the conditions
+	 *            [[a1,b1,...,z1,w1,MAX1],[a2,b2,...,z2,MAX2],...]
+	 */
 	public Simplex(final double[] c, final double[][] nb)
 	{
 		done = false;
@@ -20,93 +47,210 @@ public class Simplex
 			{
 				tableau[i][j] = nb[i][j];
 			}
+
+		zfz = tableau.length - 1;
+		q = tableau[0].length - 1;
+		b = q - 1;
 	}
 
+	/**
+	 * executes the simplex-algorithm
+	 * 
+	 * @see #simplexLogged()
+	 */
 	public void simplex()
 	{
 		if (done)
 			return;
 		else
 			done = true;
-		final int zfz = tableau.length - 1;
-		final int q = tableau[0].length - 1;
-		final int b = q - 1;
-		boolean best;
-		simplex: do
+
+		boolean best = simplexStep(true);
+		while (!best)
 		{
-			best = true;
-			besttest: for (int i = 0; i < b; i++)
-			{
-				if (tableau[zfz][i] > 0)
-				{
-					best = false;
-					break besttest;
-				}
-			}
-			if (best)
-				break simplex;
-
-			int pivotCol = 0;
-			for (int i = 0; i < b; i++)
-			{
-				if (tableau[zfz][i] > tableau[zfz][pivotCol])
-					pivotCol = i;
-			}
-
-			for (int i = 0; i < tableau.length; i++)
-			{
-				tableau[i][q] = tableau[i][b] / tableau[i][pivotCol];
-			}
-
-			int pivotRow = 0;
-			for (int i = 0; i < zfz; i++)
-			{
-				if (tableau[i][q] < tableau[pivotRow][q])
-					pivotRow = i;
-			}
-			final double pivotVal = tableau[pivotRow][pivotCol];
-
-			for (int i = 0; i < tableau.length; i++)
-			{
-				if (i == pivotRow)
-					continue;
-				final double factor = tableau[i][pivotCol] / pivotVal;
-				for (int j = 0; j < tableau[i].length; j++)
-				{
-					tableau[i][j] -= tableau[pivotRow][j] * factor;
-				}
-			}
-
-			for (int i = 0; i <= q; i++)
-			{
-				tableau[pivotRow][i] /= pivotVal;
-			}
-
-			tableau[pivotRow][q] = Double.POSITIVE_INFINITY;
-			res[pivotCol] = pivotRow;
-		} while (!best);
+			best = simplexStep(false);
+		}
 	}
 
+	/**
+	 * executes the simplex algorithm, but with logging every step as a snapshot
+	 * in a list
+	 * 
+	 * @return the log of the algorithm
+	 */
+	public List<double[][]> simplexLogged()
+	{
+		if (done)
+			return null;
+		else
+			done = true;
+
+		final List<double[][]> log = new LinkedList<>();
+		log.add(snapshot());
+		boolean best = simplexStep(true);
+		while (!best)
+		{
+			log.add(snapshot());
+			best = simplexStep(false);
+		}
+		log.add(snapshot());
+		return log;
+	}
+
+	private double[][] snapshot()
+	{
+		final double[][] sh = new double[tableau.length][tableau[0].length];
+		for (int i = 0; i < sh.length; i++)
+		{
+			for (int j = 0; j < sh[i].length; j++)
+			{
+				sh[i][j] = tableau[i][j];
+			}
+		}
+		return sh;
+	}
+
+	private boolean simplexStep(final boolean firstCheck)
+	{
+		if (firstCheck && checkBest())
+			return true;
+
+		final int pivotCol = findPivotCol();
+		calcQ(pivotCol);
+
+		final int pivotRow = findPivotRow();
+		reduceForm(pivotRow, pivotCol);
+
+		res[pivotCol] = pivotRow;
+		return checkBest();
+	}
+
+	private boolean checkBest()
+	{
+		boolean best = true;
+		besttest: for (int i = 0; i < b; i++)
+		{
+			if (tableau[zfz][i] > 0)
+			{
+				best = false;
+				break besttest;
+			}
+		}
+		return best;
+	}
+
+	private int findPivotCol()
+	{
+		int pivotCol = 0;
+		for (int i = 0; i < b; i++)
+		{
+			if (tableau[zfz][i] > tableau[zfz][pivotCol])
+				pivotCol = i;
+		}
+		return pivotCol;
+	}
+
+	private void calcQ(final int pivotCol)
+	{
+		for (int i = 0; i < tableau.length; i++)
+		{
+			if (Double.isFinite(tableau[i][q]))
+				tableau[i][q] = tableau[i][b] / tableau[i][pivotCol];
+		}
+	}
+
+	private int findPivotRow()
+	{
+		int pivotRow = 0;
+		for (int i = 0; i < zfz; i++)
+		{
+			if (tableau[i][q] < tableau[pivotRow][q])
+				pivotRow = i;
+		}
+		return pivotRow;
+	}
+
+	private void reduceForm(final int pivotRow, final int pivotCol)
+	{
+		final double pivotVal = tableau[pivotRow][pivotCol];
+		for (int i = 0; i < tableau.length; i++)
+		{
+			if (i == pivotRow)
+				continue;
+			final double factor = tableau[i][pivotCol] / pivotVal;
+			for (int j = 0; j < tableau[i].length; j++)
+			{
+				tableau[i][j] -= tableau[pivotRow][j] * factor;
+			}
+		}
+		for (int i = 0; i <= q; i++)
+		{
+			tableau[pivotRow][i] /= pivotVal;
+		}
+		tableau[pivotRow][q] = Double.POSITIVE_INFINITY;
+	}
+
+	/**
+	 * @return the maximal value of the term ax1+bx2+...+zxn under the given
+	 *         conditions. returns NaN if the algorithm was not executed yet
+	 */
 	public double getOptimum()
 	{
-		return -tableau[tableau.length - 1][tableau[tableau.length - 1].length - 2];
-	}
-	
-	public double[] getOptimalValues(){
-		final double[] ov = new double[res.length];
-		for(int i = 0; i < ov.length;i++){
-			ov[i] = tableau[res[i]][tableau[0].length-2];
-		}
-		return ov;
+		if (done)
+			return -tableau[tableau.length - 1][tableau[tableau.length - 1].length - 2];
+		else
+			return Double.NaN;
 	}
 
+	/**
+	 * @return the vector of values for the [x1, x2, ..., xn] to get the
+	 *         optimum. returns null if the algorithm was not executed yet
+	 */
+	public double[] getOptimalValues()
+	{
+		if (done)
+		{
+			final double[] ov = new double[res.length];
+			for (int i = 0; i < ov.length; i++)
+			{
+				if (Double.isInfinite(tableau[res[i]][tableau[res[i]].length - 1]))
+					ov[i] = tableau[res[i]][tableau[0].length - 2];
+				else
+					ov[i] = 0;
+			}
+			return ov;
+		} else
+			return null;
+	}
+
+	/**
+	 * returns a string-representation of the simplex-tableau
+	 */
 	@Override
 	public String toString()
 	{
+		return toString(tableau, 2);
+	}
+
+	/**
+	 * @param values
+	 *            a 2-dimensional array of values
+	 * @param precision
+	 *            the number of digits after the decimal-separator for each
+	 *            value
+	 * @return a string-representation of that array with the first dimension as
+	 *         it's height
+	 */
+	public static String toString(final double[][] values, final int precision)
+	{
 		final StringBuilder b = new StringBuilder();
-		for (int i = 0; i < tableau.length; i++)
+		for (int i = 0; i < values.length; i++)
 		{
-			b.append(Arrays.toString(tableau[i]) + "\n");
+			for (int j = 0; j < values[i].length; j++)
+			{
+				b.append(String.format("%1$+." + precision + "f\t", values[i][j]));
+			}
+			b.append("\n");
 		}
 		return b.toString();
 	}
--- Statistix/src/org/wi16/test/SimplexTest.java
+++ Statistix/src/org/wi16/test/SimplexTest.java
@@ -1,6 +1,7 @@
 package org.wi16.test;
 
 import java.util.Arrays;
+import java.util.List;
 
 import org.wi16.stat.Simplex;
 
@@ -9,17 +10,27 @@ public class SimplexTest
 
 	public static void main(String[] args)
 	{
-		// final double[] c = new double[]{1, 5};
-		// final double[][] nb = new double[][]{
-		// {5,10,1,0,0,50},
-		// {9,5,0,1,0, 45},
-		// {0,10,0,0,1,40}
-		// };
-
-		final double[] c = new double[] { 1, 2 };
-		final double[][] nb = new double[][] { { 1, 1, 1, 0, 0, 100 }, { 6, 9, 0, 1, 0, 720 }, { 0, 1, 0, 0, 1, 60 } };
+		// final double[] c = new double[] { 1, 5 };
+		// final double[][] nb = new double[][] { { 5, 10, 1, 0, 0, 50 }, { 9,
+		// 5, 0, 1, 0, 45 }, { 0, 10, 0, 0, 1, 40 } };
+		// final double[] c = new double[] { 1, 2 };
+		// final double[][] nb = new double[][] { { 1, 1, 1, 0, 0, 100 }, { 6,
+		// 9, 0, 1, 0, 720 }, { 0, 1, 0, 0, 1, 60 } };
+		// final double[] c = new double[] { 5, 4, -32, -24 };
+		// final double[][] nb = new double[][] { { 1, 3, -7, -5, 1, 0, 0, 5 },
+		// { -1, 1, 6, 5, 0, 1, 0, 3 } };
+		final double[] c = new double[] { 6, 10, 8, 12 };
+		final double[][] nb = new double[][] { { 3, 2, -1, 2, 1, 0, 0, 0, 0, 60 }, { 1, 1, 2, 1, 0, 1, 0, 0, 0, 45 },
+				{ 2, -4, -3, 4, 0, 0, 1, 0, 0, 90 }, { 4, -6, 4, 3, 0, 0, 0, 1, 0, 120 },
+				{ 1, 2, -2, 2, 0, 0, 0, 0, 1, 30 } };
 		final Simplex s = new Simplex(c, nb);
-		s.simplex();
+		final List<double[][]> log = s.simplexLogged();
+
+		for (double[][] l : log)
+		{
+			System.out.println(Simplex.toString(l, 2));
+		}
+
 		System.out.println(s.getOptimum());
 		System.out.println(Arrays.toString(s.getOptimalValues()));
 	}
